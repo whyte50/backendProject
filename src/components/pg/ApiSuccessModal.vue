a07b4c667d59540091d8649794c6b14c2b04ad1a
Emmanuel Godswill <erro50.r5@gmail.com>
2022-01-03 14:09:50 +0100

    commit

--- src/components/pg/ApiSuccessModal.vue
+++ src/components/pg/ApiSuccessModal.vue
@@ -46,21 +46,7 @@ export default {
 }
 </script>
 
-<style>
-    .modal-box {
-        position: fixed; /* Stay in place */
-        z-index: 1; /* Sit on top */
-        left: 0;
-        top: 0;
-        width: 100%; /* Full width */
-        height: 100%; /* Full height */
-        overflow: auto; /* Enable scroll if needed */
-        background-color: rgb(0,0,0); /* Fallback color */
-        background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-        display: flex;
-        align-items: center
-    }
-
+<style scoped>
     .modal-box .modal-dialog{
         width: 440px
     }
---
+++ src/components/pg/BenList.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="modal-box fade show" v-if="this.$store.state.benList">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Beneficiary Details</h4>
+                    <button type="button" class="btn-close" @click="this.$store.commit('closeBen')"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-12 bg-white rounded">
+                                <div class="table-responsive">
+                                    <table class="table table-fixed">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col" class="col-3">#</th>
+                                                <th scope="col" class="col-3">Name</th>
+                                                <th scope="col" class="col-3">Number</th>
+                                                <th scope="col" class="col-3">bankID</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody v-for="ben in benData" :key="ben.id">
+                                            <tr>
+                                                <th scope="row" class="col-3"></th>
+                                                <td class="col-3">{{ ben.accountName }}</td>
+                                                <td class="col-3">{{ ben.accountNumber }}</td>
+                                                <td class="col-3">{{ ben.bankID }}</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-light" type="button" @click="this.$store.commit('closeBen')">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from "axios"
+import store from "../../store/index"
+export default {
+    name: 'BenList',
+    data() {
+        return {
+            benData : []
+        }
+    },
+    async created() {
+        const response = await axios.get(`http://localhost:3000/payapi/account/ben_list/${store.state.userDetails.id}`)
+        console.log(response)
+        this.benData = response.data
+    }
+
+}
+</script>
+
+<style scoped>
+    .modal-box .modal-dialog{
+        width: 770px
+    }
+</style>
